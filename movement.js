2a573ebf55f816a6a539f929a2a50fcb6ee4d283
ellamegan <63962834+ellamegan@users.noreply.github.com>
2021-03-20 18:18:20 +0000

    personalimage

---
+++ movement.js
@@ -0,0 +1,17 @@
+document.addEventListener("mousemove", function (event) {
+	
+	const x = event.pageX
+	const y = event.pageY
+	
+	const target = document.querySelector(".card")
+	const targetCoords = target.getBoundingClientRect()
+	
+	const targetX = targetCoords.left + (target.offsetWidth / 2)
+	const targetY = targetCoords.top + (target.offsetHeight / 2)
+	
+	const angleX = (targetY - y)/13
+	const angleY = (targetX - x)/-13
+	
+	target.style.transform = "rotateX(" + angleX + "deg) rotateY(" + angleY + "deg)"
+	
+})
